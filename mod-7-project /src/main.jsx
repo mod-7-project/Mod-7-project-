3e97d245b5c771cf5ad28dfdd4ff5180837e7921
Abdullahk1105 <Abdullahk11051@gmail.com>
2024-08-20 16:54:58 -0400

    FINISHED PROPOSALS

--- mod-7-project /src/main.jsx
+++ mod-7-project /src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import CharacterProvider from './context/pokemonProvider.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ <CharacterProvider>
+   <App />
+ </CharacterProvider>
+  
 )
